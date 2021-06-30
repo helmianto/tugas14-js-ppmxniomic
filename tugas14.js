99c1f1ed0b28f706b7423665b725dc24e65cee9d
Helmianto <helmianto15@gmail.com>
2021-06-30 16:33:36 +0700

    intial commit

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+const pembagianUsia = 37 / 23;
+
+console.log('Hasil Bagi', pembagianUsia);
+
+console.log('Hasil Bagi dengan Math Round', Math.round(pembagianUsia));
+
+console.log('Hasil Bagi dengan Math Ceil', Math.ceil(pembagianUsia));
+
+console.log('Hasil Bagi dengan Math Floor', Math.floor(pembagianUsia));
